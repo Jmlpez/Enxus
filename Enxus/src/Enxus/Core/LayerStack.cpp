a323ceb5901ea8843a1c4f5e6d145ccaece2d4c1
Jesus <jmlopezponce@gmail.com>
2024-04-21 13:56:50 -0400

    Added OnDetach method call when popping or deleting layers in the LayerStack

--- Enxus/src/Enxus/Core/LayerStack.cpp
+++ Enxus/src/Enxus/Core/LayerStack.cpp
@@ -9,8 +9,11 @@ namespace Enxus
     }
     LayerStack::~LayerStack()
     {
-        for (auto *layer : m_Layers)
+        for (Layer *layer : m_Layers)
+        {
+            layer->OnDetach();
             delete layer;
+        }
     }
 
     void LayerStack::PushLayer(Layer *layer)
@@ -26,17 +29,19 @@ namespace Enxus
     void LayerStack::PopLayer(Layer *layer)
     {
         auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
-        if (it != m_Layers.end())
+        if (it != m_Layers.begin() + m_LayerInsertIndex)
         {
+            layer->OnDetach();
             m_Layers.erase(it);
             m_LayerInsertIndex--;
         }
     }
     void LayerStack::PopOverlay(Layer *overlay)
     {
-        auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
+        auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
         if (it != m_Layers.end())
         {
+            overlay->OnDetach();
             m_Layers.erase(it);
         }
     }
