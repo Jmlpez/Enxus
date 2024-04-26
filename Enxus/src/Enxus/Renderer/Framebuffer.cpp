51a9abf5d72b9ba1b3761d17c09e9cb3a0f64e4d
Jesus <jmlopezponce@gmail.com>
2024-04-26 08:26:52 -0400

    Solved renderbuffer problem
    The problem was that the renderer buffer was not been cleaned correctly using glDeleteRenderbuffer

--- Enxus/src/Enxus/Renderer/Framebuffer.cpp
+++ Enxus/src/Enxus/Renderer/Framebuffer.cpp
@@ -15,7 +15,7 @@ namespace Enxus
     {
         GLCall(glDeleteFramebuffers(1, &m_RendererID));
         GLCall(glDeleteTextures(1, &m_ColorAttachment));
-        GLCall(glDeleteTextures(1, &m_DepthAttachment));
+        GLCall(glDeleteRenderbuffers(1, &m_DepthAttachment));
     }
     void Framebuffer::Invalidate()
     {
@@ -24,7 +24,7 @@ namespace Enxus
         {
             GLCall(glDeleteFramebuffers(1, &m_RendererID));
             GLCall(glDeleteTextures(1, &m_ColorAttachment));
-            GLCall(glDeleteTextures(1, &m_DepthAttachment));
+            GLCall(glDeleteRenderbuffers(1, &m_DepthAttachment));
         }
 
         // GLCall(glCreateFramebuffers(1, &m_RendererID));
@@ -40,19 +40,19 @@ namespace Enxus
         GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_ColorAttachment, 0));
 
         // Using textures for Depth/Stencil attachment
-        GLCall(glCreateTextures(GL_TEXTURE_2D, 1, &m_DepthAttachment));
-        GLCall(glBindTexture(GL_TEXTURE_2D, m_DepthAttachment));
-        // GLCall(glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height));
-        GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height, 0,
-                            GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL));
-        GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_DepthAttachment, 0));
+        // GLCall(glCreateTextures(GL_TEXTURE_2D, 1, &m_DepthAttachment));
+        // GLCall(glBindTexture(GL_TEXTURE_2D, m_DepthAttachment));
+        //// GLCall(glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height));
+        // GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height, 0,
+        //                     GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL));
+        // GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_DepthAttachment, 0));
 
         // Using Render Buffer Objects for Depth/Stencil attachment (problems with ImGUI, I'm still figuring out the origin of the problem)
-        // GLCall(glGenRenderbuffers(1, &m_DepthAttachment));
-        // GLCall(glBindRenderbuffer(GL_RENDERBUFFER, m_DepthAttachment));
-        // GLCall(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height));
-        // GLCall(glBindRenderbuffer(GL_RENDERBUFFER, 0)); // unbind
-        // GLCall(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_DepthAttachment));
+        GLCall(glGenRenderbuffers(1, &m_DepthAttachment));
+        GLCall(glBindRenderbuffer(GL_RENDERBUFFER, m_DepthAttachment));
+        GLCall(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height));
+        GLCall(glBindRenderbuffer(GL_RENDERBUFFER, 0)); // unbind
+        GLCall(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_DepthAttachment));
 
         if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
         {
