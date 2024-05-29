c0390c59b70f8fc5fbc729381924f3e67da598f9
Jesus <jmlopezponce@gmail.com>
2024-05-28 22:23:18 -0400

    Setting up basic drag/drop system for the assets

--- TerrainGen/src/Panels/TerrainBiomePanel.cpp
+++ TerrainGen/src/Panels/TerrainBiomePanel.cpp
@@ -29,6 +29,7 @@ const TerrainBiomePanelProps &TerrainBiomePanel::GetPanelProps()
 void TerrainBiomePanel::OnImGuiRender()
 {
     static const char *enumTerrainTexturesArray[] = {"None", "Water", "Grass", "Rocks1", "Rocks2", "Sandy Grass", "Stony Ground", "Snow"};
+    static const int arrSize = IM_ARRAYSIZE(enumTerrainTexturesArray);
 
     if (ImGui::BeginTabItem("Biome"))
     {
@@ -54,12 +55,85 @@ void TerrainBiomePanel::OnImGuiRender()
                 ImGui::SliderFloat("Start Height", &s_Props.BiomeLayers[i].StartHeight, prevStartHeight, 1.0f);
                 ImGui::SliderFloat("Blend Strength", &s_Props.BiomeLayers[i].BlendStrength, 0.0f, 1.0f);
                 ImGui::DragFloat("Texture Scale", &s_Props.BiomeLayers[i].TextureScale, 0.001f, 0.0f, 1.0f);
-                ImGui::Combo("Texture", &s_Props.BiomeLayers[i].TextureIndex, enumTerrainTexturesArray, IM_ARRAYSIZE(enumTerrainTexturesArray));
+                // ImGui::Combo("Texture", &s_Props.BiomeLayers[i].TextureIndex, enumTerrainTexturesArray, IM_ARRAYSIZE(enumTerrainTexturesArray));
+
+                const std::string dragDropPayloadName = "Terrain Texture Index";
+                ImGui::Button(enumTerrainTexturesArray[s_Props.BiomeLayers[i].TextureIndex]);
+                if (ImGui::BeginDragDropTarget())
+                {
+                    if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload(dragDropPayloadName.c_str()))
+                    {
+                        int payloadIndex = *(const int *)payload->Data;
+                        s_Props.BiomeLayers[i].TextureIndex = payloadIndex;
+                        ImGui::EndDragDropTarget();
+                    }
+                }
             }
 
             ImGui::PopID();
         }
+
+        {
+            ImGui::Begin("Terrain Textures");
+            for (int i = 1; i < arrSize; i++)
+            {
+                const std::string dragDropPayloadName = "Terrain Texture Index";
+                ImGui::PushID(i);
+                ImGui::Button(enumTerrainTexturesArray[i]);
+                if (ImGui::BeginDragDropSource())
+                {
+                    ImGui::SetDragDropPayload(dragDropPayloadName.c_str(), &i, sizeof(int));
+                    ImGui::Text(enumTerrainTexturesArray[i]);
+                    ImGui::EndDragDropSource();
+                }
+                ImGui::PopID();
+            }
+
+            ImGui::End();
+        }
+
         ImGui::Dummy(ImVec2(0.0f, 15.0f));
         ImGui::EndTabItem();
     }
 }
+
+/*
+ImGui::Begin("Terrain Textures");
+        static const std::string buttonsLabels[] = {"Hello", "Hola", "Hallo"};
+        static int value = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            ImGui::PushID(i);
+            ImGui::Button(buttonsLabels[i].c_str(), ImVec2(60, 60));
+            if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
+            {
+                ImGui::SetDragDropPayload("DND_DEMO_CELL", &i, sizeof(int));
+                std::string name = "Dragging" + buttonsLabels[i];
+                ImGui::Text(name.c_str());
+                // Display preview (could be anything, e.g. when dragging an image we could decide to display
+                // the filename and a small preview of the image, etc.)
+
+                ImGui::EndDragDropSource();
+            }
+            ImGui::PopID();
+        }
+        ImGui::End();
+
+        for (int i = 0; i < 2; i++)
+        {
+
+            ImGui::Button(buttonsLabels[value].c_str());
+            //   ImGui::Button(buttonsLabels[value].c_str());
+            if (ImGui::BeginDragDropTarget())
+            {
+                if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("DND_DEMO_CELL"))
+                {
+                    IM_ASSERT(payload->DataSize == sizeof(int));
+                    int payload_n = *(const int *)payload->Data;
+                    value = payload_n;
+                    std::cout << value << std::endl;
+                }
+                ImGui::EndDragDropTarget();
+            }
+        }
+*/
